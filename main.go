6dded447ac207b0065d0c6a7ebc982de7514fd83
saito-hiroaki <hiro021312@gmail.com>
2022-11-23 16:57:10 +0900

    [mod]MySQLのCRUD操作のエンドポイント追加

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Users struct {
+	name  string
+	token string
+}
+
 func checkError(err error) {
 	if err != nil {
 		panic(err)
@@ -18,25 +23,14 @@ func checkError(err error) {
 
 func main() {
 
-	// res, err = sqlStatement.Exec("orange", 154)
-	// checkError(err)
-	// rowCount, err = res.RowsAffected()
-	// fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
-
-	// res, err = sqlStatement.Exec("apple", 100)
-	// checkError(err)
-	// rowCount, err = res.RowsAffected()
-	// fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
-	// fmt.Println("Done.")
-
 	http.HandleFunc("/user/create", createUser)
-	// http.HandleFunc("/user/create", getUser)
-	// http.HandleFunc("/user/create", updateUser)
+	http.HandleFunc("/user/get", getUser)
+	http.HandleFunc("/user/update", updateUser)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func createUser(w http.ResponseWriter, r *http.Request) {
+func setConnectionString() string {
 
 	err := godotenv.Load("./.env")
 	if err != nil {
@@ -51,7 +45,14 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	// Initialize connection string.
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
 
-	// Initialize connection object.
+	return connectionString
+
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+
+	connectionString := setConnectionString()
+
 	db, err := sql.Open("mysql", connectionString)
 	checkError(err)
 	defer db.Close()
@@ -59,19 +60,62 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	err = db.Ping()
 	checkError(err)
 
-	// Insert some data into table.
 	sqlStatement, err := db.Prepare("INSERT INTO users (name, token) VALUES (?, ?);")
-	res, err := sqlStatement.Exec("hoge", "fuga")
+	res, err := sqlStatement.Exec("hogehoge", "fugafuga")
 	checkError(err)
 	rowCount, err := res.RowsAffected()
 	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
 
 }
 
-// func getUser(w http.ResponseWriter, r *http.Request) {
+func getUser(w http.ResponseWriter, r *http.Request) {
 
-// }
+	connectionString := setConnectionString()
 
-// func updateUser(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", connectionString)
+	checkError(err)
+	defer db.Close()
 
-// }
+	err = db.Ping()
+	checkError(err)
+
+	// Read users table.
+	rows, err := db.Query("SELECT * FROM users;")
+	checkError(err)
+	defer rows.Close()
+	fmt.Println("Reading data:")
+
+	//TODO: 一旦100にしている
+	users := [100]Users{}
+
+	for i := 0; rows.Next(); i++ {
+		err := rows.Scan(&users[i].name, &users[i].token)
+		checkError(err)
+		fmt.Printf("Data row = (%s, %s)\n", users[i].name, users[i].token)
+	}
+
+	err = rows.Err()
+	checkError(err)
+	fmt.Println("Done.")
+
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+
+	connectionString := setConnectionString()
+
+	db, err := sql.Open("mysql", connectionString)
+	checkError(err)
+	defer db.Close()
+
+	err = db.Ping()
+	checkError(err)
+
+	// Modify some data in table.
+	rows, err := db.Exec("UPDATE users SET name = ? WHERE name = ?", "hogee", "hoge")
+	checkError(err)
+	rowCount, err := rows.RowsAffected()
+	fmt.Printf("Updated %d row(s) of data.\n", rowCount)
+	fmt.Println("Done.")
+
+}
